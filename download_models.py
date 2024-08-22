773b81631d70f53a565ebf55ea8730cc76ed3594
Player1444 <94734830+Player124413@users.noreply.github.com>
2024-08-22 11:47:46 +0300

    Add files via upload

--- download_models.py
+++ download_models.py
@@ -1,30 +1,18 @@
 import os
-from pathlib import Path
-import requests
+import subprocess
 
-RVC_other_DOWNLOAD_LINK = 'https://huggingface.co/Politrees/all_RVC-pretrained_and_other/resolve/main/other/'
-RVC_hubert_DOWNLOAD_LINK = 'https://huggingface.co/Politrees/all_RVC-pretrained_and_other/resolve/main/HuBERTs/'
+assets_folder = "./assets/"
+os.makedirs(assets_folder, exist_ok=True)
 
-now_dir = os.getcwd()
-assets_dir = os.path.join(now_dir, 'models', 'assets')
+file_links = {
+    "rmvpe/rmvpe.pt": "https://huggingface.co/Rejekts/project/resolve/main/rmvpe.pt",
+    "hubert/hubert_base.pt": "https://huggingface.co/utter-project/mHuBERT-147/resolve/main/checkpoint_best.pt"
+}
 
-
-def dl_model(link, model_name, dir_name):
-    r = requests.get(f'{link}{model_name}', stream=True)
-    r.raise_for_status()
-    with open(os.path.join(dir_name, model_name), 'wb') as f:
-        for chunk in r.iter_content(chunk_size=8192):
-            f.write(chunk)
-
-if __name__ == '__main__':
-    rvc_other_names = ['rmvpe.pt', 'fcpe.pt']
-    for model in rvc_other_names:
-        print(f'Downloading {model}...')
-        dl_model(RVC_other_DOWNLOAD_LINK, model, assets_dir)
-
-    rvc_hubert_names = ['hubert_base.pt']
-    for model in rvc_hubert_names:
-        print(f'Downloading {model}...')
-        dl_model(RVC_hubert_DOWNLOAD_LINK, model, assets_dir)
-
-    print('All models downloaded!')
+for file, link in file_links.items():
+    file_path = os.path.join(assets_folder, file)
+    if not os.path.exists(file_path):
+        try:
+            subprocess.run(['wget', '-O', file_path, link], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error downloading {file}: {e}")
